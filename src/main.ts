b97f5c529a5f098900dd1678efa48fc40e828d33
Samuel Raducan <samuel.thembp@gmail.com>
2024-03-13 10:38:00 +0000

    Add logic to display information about each pokemon card

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
 import "./styles/style.scss";
+import { pokemonArray } from "./data/pokemon";
+
+const cardContainer = document.querySelector<HTMLElement>(".card-container");
+
+if (!cardContainer) throw new Error("HTML Element does not exist...");
+
+pokemonArray.forEach((pokemon, idx) => {
+    const capitalizedPokemonName =
+        pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+    cardContainer.innerHTML += `
+        <div class="card">
+            <img class="card__image" src=${pokemon.sprite} alt=${pokemon.name} />
+            <div class="card__content">
+                <h2 class="">${capitalizedPokemonName}</h2>
+                <p>${capitalizedPokemonName} (#${idx + 1}) is a ${pokemon.types.join(" & ")} type pokemon. </p>
+            </div>
+        </div>
+    `;
+});
