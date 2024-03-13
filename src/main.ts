dc74b47d295b15e57f026b09b74fee4d69c8e417
Samuel Raducan <samuel.thembp@gmail.com>
2024-03-13 11:46:17 +0000

    Extension: add the ability to filter by available types

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { pokemonArray } from "./data/pokemon";
 import { Pokemon } from "./data/types";
 
 const cardContainer = document.querySelector<HTMLElement>(".card-container");
-const searchPokemonInput =
-    document.querySelector<HTMLInputElement>(".search-pokemon");
+const searchPokemonInputName = document.querySelector<HTMLInputElement>(
+    ".search-pokemon-by-name"
+);
+const searchPokemonInputType = document.querySelector<HTMLInputElement>(
+    ".search-pokemon-by-type"
+);
 
-if (!cardContainer || !searchPokemonInput)
+if (!cardContainer || !searchPokemonInputName || !searchPokemonInputType)
     throw new Error("HTML Element does not exist...");
 
 const renderPokemons = (pokemonArray: Pokemon[]) => {
@@ -33,12 +37,26 @@ const renderPockemonsOnPageLoad = (): void => {
     renderPokemons(pokemonArray);
 };
 
-searchPokemonInput.addEventListener("input", (event: Event) => {
+searchPokemonInputName.addEventListener("input", (event: Event) => {
     const searchByName = (event.currentTarget as HTMLInputElement).value;
 
-    const filteredPokemons = pokemonArray.filter((pokemon) =>
+    const filteredPokemon = pokemonArray.filter((pokemon) =>
         pokemon.name.toLowerCase().includes(searchByName)
     );
+
+    renderPokemons(filteredPokemon);
+});
+
+searchPokemonInputType.addEventListener("input", (event: Event) => {
+    const searchByType = (event.currentTarget as HTMLInputElement).value;
+
+    const filteredPokemons = pokemonArray.filter((pokemon) => {
+        for (const type of pokemon.types) {
+            if (type.toLowerCase().includes(searchByType)) return true;
+        }
+
+        return false;
+    });
 
     renderPokemons(filteredPokemons);
 });
