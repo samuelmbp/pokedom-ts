9c35dc552b0ccb1e915b6ca81c9b0097ede152c4
Samuel Raducan <samuel.thembp@gmail.com>
2024-03-13 11:07:06 +0000

    Extension: add the ability to search a pokemon by name

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,46 @@
 import "./styles/style.scss";
 import { pokemonArray } from "./data/pokemon";
+import { Pokemon } from "./data/types";
 
 const cardContainer = document.querySelector<HTMLElement>(".card-container");
+const searchPokemonInput =
+    document.querySelector<HTMLInputElement>(".search-pokemon");
 
-if (!cardContainer) throw new Error("HTML Element does not exist...");
+if (!cardContainer || !searchPokemonInput)
+    throw new Error("HTML Element does not exist...");
 
-pokemonArray.forEach((pokemon, idx) => {
-    const capitalizedPokemonName =
-        pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-    cardContainer.innerHTML += `
+const renderPokemons = (pokemonArray: Pokemon[]) => {
+    cardContainer.innerHTML = "";
+
+    pokemonArray.forEach((pokemon, idx) => {
+        const capitalizedPokemonName =
+            pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+        cardContainer.innerHTML += `
         <div class="card">
-            <img class="card__image" src=${pokemon.sprite} alt=${pokemon.name} />
+            <img class="card__image" src=${pokemon.sprite} 
+                alt=${pokemon.name} />
             <div class="card__content">
                 <h2 class="">${capitalizedPokemonName}</h2>
-                <p>${capitalizedPokemonName} (#${idx + 1}) is a ${pokemon.types.join(" & ")} type pokemon. </p>
+                <p>${capitalizedPokemonName} (#${idx + 1}) 
+                is a ${pokemon.types.join(" & ")} type pokemon. </p>
             </div>
         </div>
     `;
+    });
+};
+
+const renderPockemonsOnPageLoad = (): void => {
+    renderPokemons(pokemonArray);
+};
+
+searchPokemonInput.addEventListener("input", (event: Event) => {
+    const searchByName = (event.currentTarget as HTMLInputElement).value;
+
+    const filteredPokemons = pokemonArray.filter((pokemon) =>
+        pokemon.name.toLowerCase().includes(searchByName)
+    );
+
+    renderPokemons(filteredPokemons);
 });
+
+document.addEventListener("DOMContentLoaded", renderPockemonsOnPageLoad);
